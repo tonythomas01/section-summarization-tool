5198299f09e3f57140cbbf8bf2251aed87040938
Gergő Tisza <tgr.huwiki@gmail.com>
2023-05-20 18:02:28 +0300

    Wait for MediaWiki environment to load
    This will be a no-op when loaded as a userscript but necessary
    when loaded as a GreaseMonkey script.

--- script.js
+++ script.js
@@ -13,45 +13,55 @@
 // @grant        none
 // ==/UserScript==
 
-$(document).ready(function () {
+(function () {
   const GPTModel = "gpt-3.5-turbo";
   const HuggingFacesModelsMap = {
     "FacebookBartLargeCNN": "facebook/bart-large-cnn",
   };
 
-  $('.mw-headline').each(function () {
-    var sectionHeading = $(this);
-    const editLink = sectionHeading.siblings('.mw-editsection');
-    const immediateParent = sectionHeading.parent();
+  function main() {
+    $('.mw-headline').each(function () {
+      var sectionHeading = $(this);
+      const editLink = sectionHeading.siblings('.mw-editsection');
+      const immediateParent = sectionHeading.parent();
 
-    if (immediateParent.is('h2') || immediateParent.is('.mw-heading2')) {
-      var summarizeLink = $('<span>')
-        .addClass('mw-summarysection')
-        .addClass('mw-editsection')
-        .css('margin-left', '0.5em')
-        .append(
-          $('<span>')
-            .addClass('mw-editsection-bracket')
-            .text('[')
-        )
-        .append(
-          $('<a>')
-            .attr('href', '#')
-            .text('summarize')
-            .click(function (e) {
-              e.preventDefault();
-              summarizeSection(sectionHeading);
-            })
-        )
-        .append(
-          $('<span>')
-            .addClass('mw-editsection-bracket')
-            .text(']')
-        );
+      if (immediateParent.is('h2') || immediateParent.is('.mw-heading2')) {
+        var summarizeLink = $('<span>')
+          .addClass('mw-summarysection')
+          .addClass('mw-editsection')
+          .css('margin-left', '0.5em')
+          .append(
+            $('<span>')
+              .addClass('mw-editsection-bracket')
+              .text('[')
+          )
+          .append(
+            $('<a>')
+              .attr('href', '#')
+              .text('summarize')
+              .click(function (e) {
+                e.preventDefault();
+                summarizeSection(sectionHeading);
+              })
+          )
+          .append(
+            $('<span>')
+              .addClass('mw-editsection-bracket')
+              .text(']')
+          );
 
-      editLink.after(summarizeLink);
+        editLink.after(summarizeLink);
+      }
+    });
+  }
+
+  function loadMain() {
+    if (!window.$) {
+      return setTimeout(loadMain, 50);
     }
-  });
+    $(main);
+  }
+  loadMain();
 
   function fetchSummaryUsingOpenAPI(fixedPromptForChatGPT, openAPIKey, sectionText, callback) {
     const gptQuery = fixedPromptForChatGPT + sectionText;
@@ -156,4 +166,4 @@ $(document).ready(function () {
         return;
     }
   }
-});
+})();
